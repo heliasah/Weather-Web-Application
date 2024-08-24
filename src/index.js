a7926e0ab333c055e705c66575ae6f9cb664914b
heliasah <helya.sah@gmail.com>
2024-08-24 13:39:32 +0330

    Added Axios and basic js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+function updateWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML = Math.round(temperature);
+    let cityElement = document.querySelector("#weather-city");
+    cityElement.innerHTML = response.data.city;
+
+}
+
+function searchCity(city) {
+  let apiKey = "db7f142eaefbo73ffc22t50dbbc48b65";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(updateWeather);
+}
+
+
 function handleSearch(event){
     event.preventDefault();
     let searchInput = document.querySelector("#cityform-input");
     let cityElement = document.querySelector("#weather-city");
     let cityInput = searchInput.value;
-    cityInput = cityInput.charAt(0).toUpperCase() + cityInput.slice(1).toLowerCase();
 
   
     cityElement.innerHTML = cityInput;
+    searchCity(cityInput);
 
 }
 
 let searchCityElement = document.querySelector("#search-form");
 searchCityElement.addEventListener("submit",handleSearch);
+
+
+searchCity("Paris")
